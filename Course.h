7621212a737428f16f4154d39c77d3a0f2aff7f1
friedaces <friedaces@gmail.com>
2026-08-04 15:40:07 -0400

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,9 +1,9 @@
 #pragma once
 #ifndef COURSE_H
 #define COURSE_H
-
 #include <iostream>
 #include <cstring>
+using namespace std;
 
 class Course
 {
@@ -12,28 +12,22 @@ private:
 	char* courseName;
 	char* schedule;
 	double price;
-
 public:
 	Course();
 	Course(const char* cNum, const char* cName, const char* sch, double p);
 	Course(const Course& other);
 	Course& operator=(const Course& other);
 	~Course();
-
 	// Getters
 	const char* getCourseNumber() const;
 	const char* getCourseName() const;
 	const char* getSchedule() const;
 	double getPrice() const;
-
 	// setters
 	void setCourseNumber(const char* cNum);
 	void setCourseName(const char* cName);
 	void setSchedule(const char* sch);
 	void setPrice(double p);
-
 	void displayCourse() const;
-
 };
-
-#endif // !COURSE_H
+#endif
